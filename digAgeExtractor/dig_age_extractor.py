35b1995f5690b779af4e7df28b536399bcd97cf1
ZwEin <zwein27@gmail.com>
2016-10-06 16:04:56 -0700

    init extractor

---
+++ digAgeExtractor/dig_age_extractor.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: ZwEin
+# @Date:   2016-10-05 13:36:01
+# @Last Modified by:   ZwEin
+# @Last Modified time: 2016-10-05 16:01:23
+
+
+
---
+++ test/test_age_helper.py
@@ -0,0 +1,25 @@
+import sys
+import time
+import os
+import unittest
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+# TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+
+from digAgeExtractor.age_helper import *
+
+class TestAgeHelperMethods(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
