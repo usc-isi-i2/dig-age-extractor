4f0e432e9514ccaf318a65e5449d83b4f34574cf
ZwEin <zwein27@gmail.com>
2016-10-06 20:14:57 -0700

    add AgeExtractor

--- digAgeExtractor/dig_age_extractor.py
+++ digAgeExtractor/dig_age_extractor.py
@@ -1,8 +1,30 @@
 # -*- coding: utf-8 -*-
 # @Author: ZwEin
-# @Date:   2016-10-05 13:36:01
+# @Date:   2016-10-03 23:46:09
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-05 16:01:23
+# @Last Modified time: 2016-10-06 20:00:46
 
+import copy 
+import types
+from digExtractor.extractor import Extractor
+import age_helper
 
+class AgeExtractor(Extractor):
 
+    def __init__(self):
+        self.renamed_input_fields = ['text']
+
+    def extract(self, doc):
+        if 'text' in doc:
+            return age_helper.extract(doc['text'])
+        return None
+
+    def get_metadata(self):
+        return copy.copy(self.metadata)
+
+    def set_metadata(self, metadata):
+        self.metadata = metadata
+        return self
+
+    def get_renamed_input_fields(self):
+        return self.renamed_input_fields
