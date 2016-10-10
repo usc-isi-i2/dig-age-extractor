9fbfbe8e147ab798c8346614a8d085d4d3260338
ZwEin <zwein27@gmail.com>
2016-10-10 15:42:20 -0700

    remove comments

--- digAgeExtractor/age_helper.py
+++ digAgeExtractor/age_helper.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-10-05 16:01:52
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-06 22:38:53
+# @Last Modified time: 2016-10-10 15:40:34
 
 import json
 import string
@@ -14,7 +14,6 @@ from digCrfTokenizer.crf_tokenizer import CrfTokenizer
 
 def generate_age_dictionary(start=18, end=50):
     dictionary = [str(_) for _ in range(start, end + 1)]
-    # dictionary = json.dumps(dictionary, indent=4)
     return dictionary
 
 ages = generate_age_dictionary()
@@ -39,9 +38,3 @@ def extract(doc, input_fields='text', output_field='names'):
         return None
 
     return updated_doc['names'][0]['value']
-
-if __name__ == '__main__':
-    doc = "\r\n    Posted:\r\n    Saturday, June 25, 2016 1:04 AM\r\n   \n \n \n \n \r\n      \r\n\r\n      \r\n\r\n      \r\n        \r\n        \r\n          Are you A pretty thick & curvy female READY to make CHANGES with your LIFE??? TIRED of being STUCK.Well now you have the chance to earn fast money now. Im Looking for a few attractive ladies to start TODAY. First time ladies are welcome. \n \nSafety is my #1 concern!! Your safety comes First \n \n \n**Must be Reliable, Honest, Open-minded & Attractive** \nThis is for FULL or PART time work. \nWE OFFER DAY SHIFTS AND NIGHT SHIFTS. SAFETY IS OUR MAIN PRIORITY. WE ARE LOOKING FOR NON FLAKY GIRLS THAT ARE ABLE TO PROVIDE A 1 ON 1 ADULT CEXPERIENCE \n \nWE HAVE A LARGE DATA BASE OF REGULAR CLIENTS ALSO WE USE THE BEST WEBSITES OUT THERE NOT JUST CRAIGSLIST OR BP TO ADVERTISE YOU ON. EXPERIENCE IS NOT NECESSARY Just have the desire to make a lot of $$money$$ \nBUT IF YOU DO HAVE EXPERIENCE THAT IS OK. \n \nWE ARE ALSO HIRING STRIPPERS FOR BACHELOR PARTIES AND DO ADULT FILM WORK \nOPPORTUNITIES \n305-479-9468\r\n        \r\n      \r\n     \n Poster's age: 26 \n \r\n         Location:\r\n        Orlando\r\n       \n  Post ID: 12295358 orlando \n \n \n \n \n Are you A pretty thick & curvy female READY to make CHANGES with your LIFE??? TIRED of being STUCK.Well now you have the chance to earn fast money now. Im Looking for a few attractive ladies to start TODAY. First time ladies are welcome. Safety is my #1 concern!! Your safety comes First **Must be Reliable, Honest, Open-minded & Attractive** This is for FULL or PART time work. WE OFFER DAY SHIFTS AND NIGHT SHIFTS. SAFETY IS OUR MAIN PRIORITY. WE ARE LOOKING FOR NON FLAKY GIRLS THAT ARE ABLE TO PROVIDE A 1 ON 1 ADULT CEXPERIENCE WE HAVE A LARGE DATA BASE OF REGULAR CLIENTS ALSO WE USE THE BEST WEBSITES OUT THERE NOT JUST CRAIGSLIST OR BP TO ADVERTISE YOU ON. EXPERIENCE IS NOT NECESSARY Just have the desire to make a lot of $$money$$ BUT IF YOU DO HAVE EXPERIENCE THAT IS OK. WE ARE ALSO HIRING STRIPPERS FOR BACHELOR PARTIES AND DO ADULT FILM WORK OPPORTUNITIES 305-479-9468 _________ ________ _______ ___ESCORTS & STRIPPERS WANTED___ _________ _______ _______ - 26 _________ ________ _______ ___ESCORTS & STRIPPERS WANTED___ _________ _______ _______ - Orlando adult jobs - backpage.com"
-    print extract(doc)
-
-
--- test/annotate_jl.py
+++ test/annotate_jl.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-10-06 20:17:15
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-06 21:02:50
+# @Last Modified time: 2016-10-08 15:41:26
 
 import os
 import sys
@@ -14,9 +14,16 @@ TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 from digAgeExtractor.age_helper import *
 
+
+
+
 def annotate(intput_path, output_path=None):
     dataset = []
     i = 0
+    annotated_size = 0.
+    correct_size = 0.
+    assumed_annotated_size = 0.
+    assumed_correct_size = 0.
     for line in codecs.open(intput_path, 'r'):
         try:
             json_obj = json.loads(line)
@@ -29,9 +36,15 @@ def annotate(intput_path, output_path=None):
         correct = json_obj['correct'] if 'correct' in json_obj else None
         # annotated = json_obj['annotated'] if 'annotated' in json_obj else None
         annotated = extract(content)
-        annotated = annotated if annotated else []
+        # annotated = list(set(annotated)) if annotated else []
+        annotated = list(set(correct).union(set(annotated))) if annotated else []
+        
+        # correct_size += len([_ for _ in annotated if _ in correct])
+        # assumed_correct_size += len(correct)
 
- 
+        # annotated_size += len(annotated)
+        # assumed_annotated_size += len(list(set(correct).union(set(annotated))))
+
         node_text = content if content else ' '
 
         if not isinstance(node_text, basestring):
@@ -50,6 +63,9 @@ def annotate(intput_path, output_path=None):
         # .append((node_text, sorted(list(set(correct))), sorted(list(set(correct)))))
 
     # dataset = [{'content': content, 'correct': correct, 'annotated': annotated} for (content, correct, _) in dataset]
+
+    # print correct_size, 'out of', annotated_size, 'annotated ages are correct:', correct_size/annotated_size
+    # print assumed_correct_size, 'out of', assumed_annotated_size, 'annotated ages are correct:', assumed_correct_size/assumed_annotated_size
 
     if not output_path:
         output_path = intput_path
